0d3e99a96fe8fa88a6dc3143b26c1aa1a74d4b73
midorizemi <taoiwasaki@gmail.com>
2024-02-21 04:38:48 +0900

    DeviseのUserモデルを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get "home/index"
   root to: 'top#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
